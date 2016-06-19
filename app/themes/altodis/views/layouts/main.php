d1444e374e8c7abe015986f53a97596439eff97d
Egor Pisarev <egor.pisarev@gmail.com>
2016-06-20 01:00:25 +0600

    ya metr

--- app/themes/altodis/views/layouts/main.php
+++ app/themes/altodis/views/layouts/main.php
@@ -24,7 +24,34 @@ ThemeAsset::register($this);
 
 <body id="<?=Yii::$app->controller->id.'-'.Yii::$app->controller->action->id?>">
 <?php $this->beginBody() ?>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter38043080 = new Ya.Metrika({
+                    id:38043080,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
 
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/38043080" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 <!-- Preloader -->
 <div class="page-loader" style="display: none;">
     <div class="loader" style="display: none;">Loading...</div>
